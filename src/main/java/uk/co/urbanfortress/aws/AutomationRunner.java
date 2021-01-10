eb4671b4f64c55331132c1619a738d93b4046f26
Murray Tait <6074789+deathtumble@users.noreply.github.com>
2021-01-10 14:38:10 +0000

    Allow piped passwords

--- src/main/java/uk/co/urbanfortress/aws/AutomationRunner.java
+++ src/main/java/uk/co/urbanfortress/aws/AutomationRunner.java
@@ -4,6 +4,7 @@ import java.io.Console;
 import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
+import java.util.Scanner;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -20,58 +21,77 @@ import org.springframework.stereotype.Component;
 @ConditionalOnProperty(prefix = "job.autorun", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class AutomationRunner implements CommandLineRunner {
 
-	private CredentialsRetriever credentialsRetriever;
-	private CredentialsWriter credentialsWriter;
-	private CommandLineParser parser;
+  private CredentialsRetriever credentialsRetriever;
+  private CredentialsWriter credentialsWriter;
+  private CommandLineParser parser;
 
-	public AutomationRunner(CredentialsRetriever credentialsRetriever, CredentialsWriter credentialsWriter) {
-		super();
-		this.credentialsRetriever = credentialsRetriever;
-		this.credentialsWriter = credentialsWriter;
-		parser = new DefaultParser();
-	}
+  public AutomationRunner(CredentialsRetriever credentialsRetriever, CredentialsWriter credentialsWriter) {
+    super();
+    this.credentialsRetriever = credentialsRetriever;
+    this.credentialsWriter = credentialsWriter;
+    parser = new DefaultParser();
+  }
 
-	@Override
-	public void run(String... args) throws Exception {
-		Option usernameOption = new Option("u", "username", true, "sso username");
-		usernameOption.setRequired(true);
-		Option portalUrlOption = new Option("s", "ssoportal", true, "sso portal url");
-		portalUrlOption.setRequired(true);
-		Option credentialsFileOption = new Option("c", "credentials", true, "aws credentials file");
-		credentialsFileOption.setRequired(true);
-		Option passwordOption = new Option("p", "password", true, "sso password");
-		passwordOption.setRequired(false);
+  @Override
+  public void run(String... args) throws Exception {
+    Option usernameOption = new Option("u", "username", true, "sso username");
+    usernameOption.setRequired(true);
+    Option portalUrlOption = new Option("s", "ssoportal", true, "sso portal url");
+    portalUrlOption.setRequired(true);
+    Option credentialsFileOption = new Option("c", "credentials", true, "aws credentials file");
+    credentialsFileOption.setRequired(true);
+    Option pipedPasswordOption = new Option("P", "pipedPassword", false, "piped sso password");
+    pipedPasswordOption.setRequired(false);
+    Option passwordOption = new Option("p", "password", false, "sso password");
+    passwordOption.setRequired(false);
+    Options options = new Options().addOption(usernameOption).addOption(portalUrlOption)
+        .addOption(credentialsFileOption).addOption(passwordOption).addOption(pipedPasswordOption);
 
-		Options options = new Options().addOption(usernameOption).addOption(portalUrlOption)
-				.addOption(credentialsFileOption).addOption(passwordOption);
+    try {
+      CommandLine cmd = parser.parse(options, args);
 
-		try {
-			CommandLine cmd = parser.parse(options, args);
+      String password;
+      if (cmd.hasOption(pipedPasswordOption.getOpt())) {
+        password = getPipedPassword();
+      } else if (cmd.hasOption(passwordOption.getOpt())) {
+        password = cmd.getOptionValue(passwordOption.getOpt());
+      } else {
+        password = getPromptedPassword();
+      }
 
-			String password = cmd.getOptionValue(passwordOption.getOpt());
-			if (password == null) {
-				password = getPassword();
-			}
-			String portalUrl = cmd.getOptionValue(portalUrlOption.getOpt());
-			String username = cmd.getOptionValue(usernameOption.getOpt());
-			String credentialsFile = cmd.getOptionValue(credentialsFileOption.getOpt());
+      String portalUrl = cmd.getOptionValue(portalUrlOption.getOpt());
+      String username = cmd.getOptionValue(usernameOption.getOpt());
+      String credentialsFile = cmd.getOptionValue(credentialsFileOption.getOpt());
 
-			List<Properties> credentailsCollection = credentialsRetriever.retrieveAllCredentials(username, password,
-					portalUrl);
+      List<Properties> credentailsCollection = credentialsRetriever.retrieveAllCredentials(username, password,
+          portalUrl);
 
-			credentialsWriter.writeAllCredentials(credentialsFile, credentailsCollection);
-		} catch (ParseException exp) {
-			HelpFormatter formatter = new HelpFormatter();
-			formatter.printHelp("credentials", options);
-		} catch (ApplicationException exp) {
-			System.err.print(exp.getMessage());
-			throw exp; 
- 		}
-	}
+      credentialsWriter.writeAllCredentials(credentialsFile, credentailsCollection);
+    } catch (ParseException exp) {
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp("credentials", options);
+    } catch (ApplicationException exp) {
+      System.err.print(exp.getMessage());
+      throw exp;
+    }
+  }
 
-	private String getPassword() throws IOException {
-		Console console = System.console();
-		char[] passwordArray = console.readPassword("Enter your secret password: ");
-		return new String(passwordArray);
-	}
+  private String getPipedPassword() {
+    String result = "";
+
+    Scanner input = new Scanner(System.in);
+    if (input.hasNext()) {
+      result = input.nextLine();
+
+    }
+    input.close();
+
+    return result;
+  }
+
+  private String getPromptedPassword() throws IOException {
+    Console console = System.console();
+    char[] passwordArray = console.readPassword("Enter your secret password: ");
+    return new String(passwordArray);
+  }
 }
